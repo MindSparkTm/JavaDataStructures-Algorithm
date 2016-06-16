7ddc4f4930e9322fac00072921e8b704e9657240
MindSparkTm <smartsurajit2008@gmail.com>
2016-06-16 15:39:45 +0300

    Update

--- PairingProblem.java
+++ PairingProblem.java
@@ -1,14 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class PairingProblem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner input = new Scanner(System.in);
+		FastScanner input = new FastScanner(System.in);
 		
 		int n;
-		System.out.println("Enter the number of integers");
 		n = input.nextInt();
 		
 		int [] ar = new int [n];
@@ -20,24 +25,16 @@ public class PairingProblem {
 		}
 			
 		
-		for(int i = n -1; i>0;i--)
-		{
-			for(int j = 0;j<i;j++)
-			{
-				if(ar[j] >ar[j+1])
-				{
-					swap(j,j+1,ar);
-				}
-			}
-		}
+		Arrays.sort(ar);
 		
 		for(int i =0 ;i<n;i++)
 		{
-			System.out.println(ar[i]);
 			
 			if(i == (n-1))
 			{
-				System.out.println("The product is " + (ar[n-1]*ar[n-2]));
+				long a = (long)ar[n-1]* (long)ar[n-2];
+				
+				System.out.println(a);
 			}
 		}
 		
@@ -45,13 +42,35 @@ public class PairingProblem {
 		
 	}
 	
-	public static void swap(int x,int y,int [] a)
-	{
-		int temp;
-		temp = a[x];
-		a[x] = a[y];
-		a[y] = temp;
-		
-	}
+
+	
+	 static class FastScanner {
+	        BufferedReader br;
+	        StringTokenizer st;
+
+	        FastScanner(InputStream stream) {
+	            try {
+	                br = new BufferedReader(new InputStreamReader(stream));
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	        }
+
+	        String next() {
+	            while (st == null || !st.hasMoreTokens()) {
+	                try {
+	                    st = new StringTokenizer(br.readLine());
+	                } catch (IOException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	            return st.nextToken();
+	        }
+
+	        int nextInt() {
+	            return Integer.parseInt(next());
+	        }
+	    }
+
 
 }
